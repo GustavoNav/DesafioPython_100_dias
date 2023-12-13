4d116578211464227ab7b8df0b4e1369df56dde0
Gustavo Navarro Felix <106482561+GustavoNav@users.noreply.github.com>
2023-12-13 02:18:24 +0000

    dia 32

---
+++ dia_032.py
@@ -0,0 +1,32 @@
+#Dia 32 - Subsets
+#Link para o Desafio: https://leetcode.com/problems/subsets/description/
+
+#Enunciado:
+'''
+Given an integer array nums of unique elements, return all possible 
+subsets
+ (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+'''
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            temp = []
+            for curr in result:
+                temp.append(curr + [num])
+            result += temp
+        return result
