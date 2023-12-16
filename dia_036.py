a97f69f494ff4e136d2b855d213816940ae7a152
Gustavo Navarro Felix <106482561+GustavoNav@users.noreply.github.com>
2023-12-16 21:09:29 +0000

    dia 36

---
+++ dia_036.py
@@ -0,0 +1,61 @@
+#Dia 36 - Remove Duplicates from Sorted Array
+#Link para o Desafio: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+#Enunciado:
+'''
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        aux = {}
+        for i in range(n):
+            if nums[i] not in aux:
+                aux[nums[i]] = 0
+            else:
+                aux[nums[i]] += 1
+        
+        rem = []
+        for x,y in aux.items():
+            if y > 0:
+                while y > 0:
+                    rem.append(x)
+                    y -= 1
+
+        while rem:
+            nums.remove(rem[0])
+            rem.pop(0)
