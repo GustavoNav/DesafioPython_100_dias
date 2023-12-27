6f69907757397850d66b43cb31d50757e87a981d
Gustavo Navarro Felix <106482561+GustavoNav@users.noreply.github.com>
2023-12-27 01:10:40 +0000

    dia 41

---
+++ dia_041.py
@@ -0,0 +1,47 @@
+#Dia 41 - 3Sum
+#Link para o Desafio: https://leetcode.com/problems/3sum/
+
+#Enunciado:
+
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        result = []
+        aux = []
+        for i in range(n): 
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        aux = sorted([nums[i], nums[j], nums[k]])
+                    
+                    if aux not in result and aux != []:
+                        result.append(aux)
+        return result
